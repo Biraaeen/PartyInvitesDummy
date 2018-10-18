f7029063918a63e158bb66de964f44d22cca7b8b
Biraaeen <biraveenponnu@hotmail.com>
2018-10-18 23:52:08 +0200

    Added validation for the GuestResponse model

--- WebApplication1/Models/GuestResponse.cs
+++ WebApplication1/Models/GuestResponse.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter yout email adress")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage ="Please enter a valid email adress")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter yout phone number")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify wheter you'll attend")]
         public bool? WillAttend { get; set; }
     }
 }
